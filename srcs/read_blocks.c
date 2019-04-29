b74eb188a8e8f5519a2f688a821763d5903080c9
Christian Mouyeme <christianmouyeme@gmail.com>
2019-04-29 18:52:38 +0200

    Ajout du GNL et debut du read_blocks

--- srcs/read_blocks.c
+++ srcs/read_blocks.c
@@ -6,11 +6,34 @@
 /*   By: cmouyeme <cmouyeme@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/26 22:55:27 by cmouyeme          #+#    #+#             */
-/*   Updated: 2019/04/29 16:50:20 by cmouyeme         ###   ########.fr       */
+/*   Updated: 2019/04/29 18:52:08 by cmouyeme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
+#include "../libft/libft.h"
 #include <unistd.h>
 
+char	*read_tetriminos(int fd)
+{
+	// char	*str = NULL;
+	int		ret;
+	char	buff[BUFF_SIZE + 1];
 
+	ret = 0;
+	while ((ret = read(fd, buff, BUFF_SIZE) > 0) && (ret != -1))
+	{
+		buff[ret] = '\0';
+		if (!str)
+			ft_strnew(0);
+
+	}
+}
+
+int		analyse_tetriminos(char *str)
+{
+	int		i;
+	char	*line;
+	
+
+}
