9fb70693e46fd37eeb642978df28ce12ec86d0e6
Christian Mouyeme <christianmouyeme@gmail.com>
2019-05-02 22:48:20 +0200

    Enlever le BUFF_SIZE dans le .h

--- srcs/fillit.h
+++ srcs/fillit.h
@@ -6,14 +6,12 @@
 /*   By: cmouyeme <cmouyeme@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/26 23:09:16 by gdalard           #+#    #+#             */
-/*   Updated: 2019/04/29 17:28:54 by cmouyeme         ###   ########.fr       */
+/*   Updated: 2019/05/02 22:47:44 by cmouyeme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLIT_H
 # define FILLIT_H
-
-#define BUFF_SIZE 21
 
 char	*read_blocks(int fd);
 
